719dc57b8d714a348e044ca33f5d142777dea356
yogesh kumar saini <105904333+Ktkmina@users.noreply.github.com>
2024-08-05 17:58:36 +0530

    math and num

---
+++ day5/numsmath.js
@@ -0,0 +1,40 @@
+/*const score=100
+
+const balance = new Number(100)
+//console.log(balance)
+//console.log(score)
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+
+
+const price = 23.58964
+console.log(price.toPrecision(3))
+
+const rate=1000000
+console.log(rate.toLocaleString('en-IN'))
+
+console.log(Number.MAX_VALUE)*/
+
+
+
+//*+*+*+*+*+*+ mathe Library**+*+*+*+*+
+/*console.log(Math)
+console.log(Math.abs(4))
+console.log(Math.abs(-4))
+console.log(Math.round(4,6))
+console.log(Math.ceil(4,2))
+console.log(Math.floor(4,2))
+console.log(Math.min(1,2,3,4,5))
+console.log(Math.max(1,2,3,4,5))*/
+
+console.log(Math.random())
+console.log((Math.random()*10)+1)
+console.log(Math.floor(Math.random()*10)+1)
+
+
+const min=10
+const max=20
+
+Math.floor(Math.random(max-min+1)+min)
+console.log(Math.floor(Math.random(max-min+1)+min))
