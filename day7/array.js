5af963ed86d84b59b96431ee7096d26ecdda19ed
yogesh kumar saini <105904333+Ktkmina@users.noreply.github.com>
2024-08-07 15:17:54 +0530

    arr

---
+++ day7/array.js
@@ -0,0 +1,20 @@
+// Arrays
+const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const heros= ["ironman","spiderman","thor","hulk","captain"]
+
+const Arr2= new Array(1,2,3,4,5,6,7,8,9,10)
+//console.log(Arr2[2])
+
+// array methods
+
+
+//Arr2.push(6)
+
+//Arr2.unshift(9)
+//Arr2.shift()
+//console.log(Arr2)
+
+console.log(Arr.includes(9))
+
+console.log(Arr2)
