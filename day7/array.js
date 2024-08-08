4b28d57df6aa90388d39714e0b50d460b61a746b
yogesh kumar saini <105904333+Ktkmina@users.noreply.github.com>
2024-08-08 15:12:45 +0530

    arrays update

--- day7/array.js
+++ day7/array.js
@@ -1,7 +1,7 @@
 // Arrays
-const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const myArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-const heros= ["ironman","spiderman","thor","hulk","captain"]
+//const heros= ["ironman","spiderman","thor","hulk","captain"]
 
 const Arr2= new Array(1,2,3,4,5,6,7,8,9,10)
 //console.log(Arr2[2])
@@ -15,6 +15,42 @@ const Arr2= new Array(1,2,3,4,5,6,7,8,9,10)
 //Arr2.shift()
 //console.log(Arr2)
 
-console.log(Arr.includes(9))
+//console.log(Arr.includes(9))
 
-console.log(Arr2)
+//console.log(Arr2)
+
+
+//slice adn splice
+
+/*console.log("A", myArr)
+const myn1 = myArr.slice(1,3)
+const myn2 = myArr.splice(1,3)
+
+console.log(myn1)
+console.log(myn2)
+console.log("B",myArr)
+console.log("c",myArr)*/
+
+const heros = ["ironman", "spiderman", "thor", "hulk", "captain"]
+const dc = ["superman","batman","wonderwoman"]  
+//heros.push(dc)
+//let totalHeros=heros.concat(dc)
+
+//console.log(totalHeros)
+
+let totalHeros = [...heros,...dc]
+//console.log(totalHeros)
+
+const anotherarr= [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+let bArr= anotherarr.flat(Infinity)
+console.log(bArr)
+
+//console.log(Array.isArray("yogesh"))
+//console.log(Array.from("yogesh"))
+//console.log(Array.from({name:"yogesh"}))
+
+
+let A=105
+let B=205
+let C=405
+console.log(Array.of(A,B,C))
